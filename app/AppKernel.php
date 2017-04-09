ee43539bc9bcc48584b40e38b15b564ad93109ec
fhuszti <f.huszti@gmail.com>
2017-04-09 18:22:45 +0200

    Added CraueFormFlowBundle for multi-steps form

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new OTS\BillingBundle\OTSBillingBundle(),
             new OTS\CoreBundle\OTSCoreBundle(),
+            new Craue\FormFlowBundle\CraueFormFlowBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
