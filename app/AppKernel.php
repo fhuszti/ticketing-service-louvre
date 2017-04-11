e0e83c720996ef96ea5867084a26ca3cc067b111
fhuszti <f.huszti@gmail.com>
2017-04-11 03:37:36 +0200

    Removed CraueFormFlowBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new OTS\BillingBundle\OTSBillingBundle(),
             new OTS\CoreBundle\OTSCoreBundle(),
-            new Craue\FormFlowBundle\CraueFormFlowBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
