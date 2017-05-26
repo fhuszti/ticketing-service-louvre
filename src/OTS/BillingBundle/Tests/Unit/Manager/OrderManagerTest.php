f921857266d794823ab12a5e9a82c2f300b85f89
fhuszti <f.huszti@gmail.com>
2017-05-26 19:25:18 +0200

    Finished unit testing on a couple OrderManager methods

--- src/OTS/BillingBundle/Tests/Unit/Manager/OrderManagerTest.php
+++ src/OTS/BillingBundle/Tests/Unit/Manager/OrderManagerTest.php
@@ -3,10 +3,46 @@ namespace OTS\BillingBundle\Tests\Unit\Manager;
 
 use OTS\BillingBundle\Manager\OrderManager;
 use OTS\BillingBundle\Entity\Ticket;
+use Doctrine\Common\Collections\ArrayCollection;
 use PHPUnit\Framework\TestCase;
 
 class OrderManagerTest extends TestCase {
+    public function generateOrderManager() {
+        $translator =  $this->getMockBuilder('Symfony\Component\Translation\TranslatorInterface')
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $validator =   $this->getMockBuilder('Symfony\Component\Validator\Validator\RecursiveValidator')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+        $errorReturn = $this->getMockBuilder('OTS\BillingBundle\Service\BillingForm\ErrorReturn')
+                            ->disableOriginalConstructor()
+                            ->getMock();
+
+        return new OrderManager( $translator, $validator, $errorReturn );
+    }
+
+    public function generateTicket($date, $discounted = false) {
+        $ticket = new Ticket();
+        $ticket->setDiscounted( $discounted );
+        $ticket->setBirthDate( $date );
+
+        return $ticket;
+    }
+
+
+
+
+
     
+
+    public function ticketAssertion($date, $expected) {
+        $orderManager = $this->generateOrderManager();
+
+        $ticket = $this->generateTicket($date);
+        
+        return $this->assertEquals( $expected, $orderManager->checkTicketPrice($ticket) );
+    }
+
     public function testCheckTicketPrice() {
         $dates = array(
         	'toddlerRate' => new \DateTime('-2 years'),
@@ -24,22 +60,69 @@ class OrderManagerTest extends TestCase {
 
 
 
-    public function ticketAssertion($date, $expected) {
-    	$translator =  $this->getMockBuilder('Symfony\Component\Translation\TranslatorInterface')
-                           ->disableOriginalConstructor()
-                           ->getMock();
-        $validator =   $this->getMockBuilder('Symfony\Component\Validator\Validator\RecursiveValidator')
-                          ->disableOriginalConstructor()
-                          ->getMock();
-        $errorReturn = $this->getMockBuilder('OTS\BillingBundle\Service\BillingForm\ErrorReturn')
-                            ->disableOriginalConstructor()
-                            ->getMock();
 
-        $orderManager = new OrderManager( $translator, $validator, $errorReturn );
 
-        $ticket = new Ticket();
-		$ticket->setBirthDate( $date );
+
+    public function generateTicketData() {
+        $data = array(
+            array(
+                'date' => new \DateTime('-67 years'),
+                'discounted' => false
+            ),
+            array(
+                'date' => new \DateTime('-19 years'),
+                'discounted' => true
+            ),
+            array(
+                'date' => new \DateTime('-2 years'),
+                'discounted' => false
+            )
+        );
+
+        return $data;
+    }
+
+    public function generateTicketCollection( $tickets = array() ) {
+        $ticketsCollection = new ArrayCollection();
+
+        if ( !empty($tickets) ) {
+            foreach( $tickets as $ticket ) {
+                if ( is_array($ticket) ) {
+                    $ticketsCollection[] = $this->generateTicket( $ticket['date'], $ticket['discounted'] );
+                }
+            }
+        }
+
+        return $ticketsCollection;
+    }
+
+    public function testManageTotalPrice() {
+        $orderManager = $this->generateOrderManager();
+
         
-        return $this->assertEquals( $expected, $orderManager->checkTicketPrice($ticket) );
+
+        //empty test
+        $tickets = $this->generateTicketCollection();
+
+        $order = $this->getMockBuilder('OTS\BillingBundle\Entity\TicketOrder')
+                      ->disableOriginalConstructor()
+                      ->getMock();
+
+        $this->assertEquals( 0, $orderManager->manageTotalPrice($tickets, $order) );
+
+
+
+        //filled test
+        $tickets = $this->generateTicketCollection( $this->generateTicketData() );
+
+        $order = $this->getMockBuilder('OTS\BillingBundle\Entity\TicketOrder')
+                      ->disableOriginalConstructor()
+                      ->getMock();
+
+        $order->expects( $this->any() )
+              ->method('getType')
+              ->will( $this->returnValue(true) );
+
+        $this->assertEquals( 22, $orderManager->manageTotalPrice($tickets, $order) );
     }
 }
